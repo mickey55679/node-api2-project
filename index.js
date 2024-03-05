076b046db5da6550bfb513a7e67912274780e3cc
Michaiah Bos <michaiahbos@yahoo.com>
2024-03-05 13:39:16 -0600

    did server in server.js

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
 // require your server and launch it here
-const express = require('express');
-
-const server = express();
-
-server.use('/', (req, res) => {
-    res.status(200).send('Hello from express')
-})
-
-server.listen(9000, () => {
-    console.log('Magic happening on port 9000')
-})
