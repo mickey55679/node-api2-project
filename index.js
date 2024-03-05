cebc8a9f550ce017e4e79ec6343fa3c18f37ea44
Michaiah Bos <michaiahbos@yahoo.com>
2024-03-05 00:15:01 -0600

    set up server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // require your server and launch it here
+const express = require('express');
+
+const server = express();
+
+server.use('/', (req, res) => {
+    res.status(200).send('Hello from express')
+})
+
+server.listen(9000, () => {
+    console.log('Magic happening on port 9000')
+})
